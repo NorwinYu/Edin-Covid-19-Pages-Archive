c3b1e675e1da680b4505b115333f13e5f7723246
Norwin Yu <norwinyu@gmail.com>
2020-04-01 22:15:51 +0100

    Update for web pages structure changes

--- download.sh
+++ download.sh
@@ -2,8 +2,9 @@ wget \
   --execute="robots = off" \
   --mirror \
   --convert-links \
-  --no-parent \
   --wait=5 \
   --html-extension \
+  --no-parent \
+  --level=1 \
   https://www.ed.ac.uk/news/covid-19
 date > date.txt
